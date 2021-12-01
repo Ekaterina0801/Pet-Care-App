b2d6abdf764b57fe40454038da5106b781699a89
Ekaterina0801 <ds.katrin@mail.ru>
2021-12-02 00:59:38 +0300

    Отредактированы заметки, изменен шрифт в виджетах паспорта

--- lib/pages/ProfilePage/Passport.dart
+++ lib/pages/ProfilePage/Passport.dart
@@ -16,129 +16,9 @@ class Passport extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         InfoWidget("Владелец", nameowner),
-        InfoWidget("Дата рождения", dateb),
-        //InfoWidget("Порода", breed),
-        //InfoWidget("", info)
+        InfoWidget("Дата рождения питомца", dateb),
         InfoWidget("Прививки", vac),
         InfoWidget("Болезни", ill),
-        // Container(
-        //     // width: MediaQuery.of(context).size.width * 0.25,
-        //     // height: MediaQuery.of(context).size.height * 0.05,
-        //     padding: EdgeInsets.only(top: 15),
-        //     margin: EdgeInsets.only(bottom: 5),
-        //     decoration: BoxDecoration(
-        //       borderRadius: BorderRadius.circular(10),
-        //       color: Color.fromRGBO(240, 240, 240, 1),
-        //     ),
-        //     child: Row(
-        //       children: [
-        //         Container(
-        //             //padding: EdgeInsets.only(top: 30, bottom: 30),
-        //             child: Text(
-        //           'Владелец: $nameowner',
-        //           textAlign: TextAlign.left,
-        //           style: GoogleFonts.comfortaa(
-        //               fontStyle: FontStyle.normal,
-        //               fontWeight: FontWeight.w800,
-        //               fontSize: 14),
-        //         )),
-        //       ],
-        //     )),
-        // Container(
-        //     // width: MediaQuery.of(context).size.width * 0.25,
-        //     // height: MediaQuery.of(context).size.height * 0.05,
-        //     padding: EdgeInsets.only(top: 15),
-        //     margin: EdgeInsets.only(top: 5, bottom: 5),
-        //     decoration: BoxDecoration(
-        //       borderRadius: BorderRadius.circular(10),
-        //       color: Color.fromRGBO(240, 240, 240, 1),
-        //     ),
-        //     child: Container(
-        //         //padding: EdgeInsets.only(top: 35),
-        //         child: Text(
-        //       'Дата рождения: $dateb',
-        //       textAlign: TextAlign.left,
-        //       style: GoogleFonts.comfortaa(
-        //           fontStyle: FontStyle.normal,
-        //           fontWeight: FontWeight.w800,
-        //           fontSize: 14),
-        //     ))),
-        // Container(
-        //     // width: MediaQuery.of(context).size.width * 0.25,
-        //     // height: MediaQuery.of(context).size.height * 0.05,
-        //     padding: EdgeInsets.only(top: 15),
-        //     margin: EdgeInsets.only(top: 5, bottom: 5),
-        //     decoration: BoxDecoration(
-        //       borderRadius: BorderRadius.circular(10),
-        //       color: Color.fromRGBO(240, 240, 240, 1),
-        //     ),
-        //     child: Container(
-        //         //padding: EdgeInsets.only(top: 35),
-        //         child: Text(
-        //       'Порода: $breed',
-        //       textAlign: TextAlign.left,
-        //       style: GoogleFonts.comfortaa(
-        //           fontStyle: FontStyle.normal,
-        //           fontWeight: FontWeight.w800,
-        //           fontSize: 14),
-        //     ))),
-        // Container(
-        //     // width: MediaQuery.of(context).size.width * 0.25,
-        //     // height: MediaQuery.of(context).size.height * 0.05,
-        //     padding: EdgeInsets.only(top: 15),
-        //     margin: EdgeInsets.only(top: 5, bottom: 5),
-        //     decoration: BoxDecoration(
-        //       borderRadius: BorderRadius.circular(10),
-        //       color: Color.fromRGBO(240, 240, 240, 1),
-        //     ),
-        //     child: Container(
-        //         //padding: EdgeInsets.only(top: 35),
-        //         child: Text(
-        //       'Окрас: $color',
-        //       textAlign: TextAlign.left,
-        //       style: GoogleFonts.comfortaa(
-        //           fontStyle: FontStyle.normal,
-        //           fontWeight: FontWeight.w800,
-        //           fontSize: 14),
-        //     ))),
-        // Container(
-        //     //width: MediaQuery.of(context).size.width * 0.25,
-        //     //height: MediaQuery.of(context).size.height * 0.05,
-        //     padding: EdgeInsets.only(top: 15),
-        //     margin: EdgeInsets.only(top: 5, bottom: 5),
-        //     decoration: BoxDecoration(
-        //       borderRadius: BorderRadius.circular(10),
-        //       color: Color.fromRGBO(240, 240, 240, 1),
-        //     ),
-        //     child: Container(
-        //         //padding: EdgeInsets.only(top: 35),
-        //         child: Text(
-        //       'Вакцины и прививки: $vac',
-        //       textAlign: TextAlign.left,
-        //       style: GoogleFonts.comfortaa(
-        //           fontStyle: FontStyle.normal,
-        //           fontWeight: FontWeight.w800,
-        //           fontSize: 14),
-        //     ))),
-        // Container(
-        //     // width: MediaQuery.of(context).size.width * 0.25,
-        //     // height: MediaQuery.of(context).size.height * 0.05,
-        //     padding: EdgeInsets.only(top: 15),
-        //     margin: EdgeInsets.only(top: 5, bottom: 5),
-        //     decoration: BoxDecoration(
-        //       borderRadius: BorderRadius.circular(10),
-        //       color: Color.fromRGBO(240, 240, 240, 1),
-        //     ),
-        //     child: Container(
-        //         //padding: EdgeInsets.only(top: 35),
-        //         child: Text(
-        //       'Истрия болезней и операций: $ill',
-        //       textAlign: TextAlign.left,
-        //       style: GoogleFonts.comfortaa(
-        //           fontStyle: FontStyle.normal,
-        //           fontWeight: FontWeight.w800,
-        //           fontSize: 14),
-        //     )))
       ],
     );
   }
@@ -163,8 +43,8 @@ class InfoWidget extends StatelessWidget {
                   title,
                   style: GoogleFonts.comfortaa(
                       fontStyle: FontStyle.normal,
-                      fontWeight: FontWeight.w800,
-                      fontSize: 16),
+                      fontWeight: FontWeight.w900,
+                      fontSize: 17),
                 ),
                 subtitle: Text(
                   info,
@@ -172,20 +52,11 @@ class InfoWidget extends StatelessWidget {
                       color: Colors.black,
                       fontStyle: FontStyle.normal,
                       fontWeight: FontWeight.w800,
-                      fontSize: 14),
+                      fontSize: 15),
                 ),
+                leading: Icon(Icons.edit),
                 isThreeLine: true,
               )),
-          // child: RichText(
-          //   text: TextSpan(children: <InlineSpan>[
-          //     TextSpan(
-          //         text: name + '\n',
-          //         style: Theme.of(context).textTheme.bodyText2),
-          //     TextSpan(
-          //         text: facultyandcourse + '\n',
-          //         style: Theme.of(context).textTheme.bodyText1),
-          //   ]),
-          // ),
         )
       ]),
     );
