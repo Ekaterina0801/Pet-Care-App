963ebebea9835e65a94c612549e7b50a4aa1a9a8
Анна Шишкина <shis@sfedu.ru>
2021-12-02 18:17:08 +0300

    Отображение информации о передержке, добавление пункта цены, кнопка

--- lib/pages/PetBoardingPage/Account.dart
+++ lib/pages/PetBoardingPage/Account.dart
@@ -3,7 +3,13 @@ class Account {
   final String email;
   final String district;
   final String kinfofpet;
+  final String price;
   final bool agreement;
   Account(
-      {this.name, this.email, this.district, this.kinfofpet, this.agreement});
+      {this.name,
+      this.email,
+      this.district,
+      this.kinfofpet,
+      this.price,
+      this.agreement});
 }
