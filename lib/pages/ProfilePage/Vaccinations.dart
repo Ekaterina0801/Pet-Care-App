090624a034ca97e3e570aa14af46a1e240379e39
Анна Шишкина <shis@sfedu.ru>
2022-03-25 22:07:56 +0300

    Сделаны отступы в Vaccinations

--- lib/pages/ProfilePage/Vaccinations.dart
+++ lib/pages/ProfilePage/Vaccinations.dart
@@ -4,23 +4,33 @@ import 'package:google_fonts/google_fonts.dart';
 
 import '../BasePage.dart';
 
-List<String> photos = ["./assets/images/article_2.6.jpg","./assets/images/article_1.2.jpg","./assets/images/article_1.1.jpg"];
-List<String> date = ["01.02.2021","01.04.2021","12.05.2021"];
-List<String> titles = ["Прививка от бешенства", "Другая важная прививка", "Третья важная прививка"];
+List<String> photos = [
+  "./assets/images/article_2.6.jpg",
+  "./assets/images/article_1.2.jpg",
+  "./assets/images/article_1.1.jpg"
+];
+List<String> date = ["01.02.2021", "01.04.2021", "12.05.2021"];
+List<String> titles = [
+  "Прививка от бешенства",
+  "Другая важная прививка",
+  "Третья важная прививка"
+];
 
 class VaccinationsPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return BasePage(
       title: "Прививки",
       body: ListView.builder(
         itemCount: photos.length,
-  itemBuilder: (context, index) {
-      return  VaccinationsCard(photos[index],date[index],titles[index]);
-      
-        
-  }),
+        itemBuilder: (context, index) {
+          return Padding(
+            child: VaccinationsCard(photos[index], date[index], titles[index]),
+            padding: EdgeInsets.symmetric(vertical: 20),
+          );
+        },
+        padding: EdgeInsets.all(10),
+      ),
     );
   }
 }
@@ -29,10 +39,9 @@ class VaccinationsCard extends StatelessWidget {
   String image;
   String date;
   String title;
-  
-  VaccinationsCard(String image, String date, String title)
-  {
-    this.image=image;
+
+  VaccinationsCard(String image, String date, String title) {
+    this.image = image;
     this.date = date;
     this.title = title;
   }
@@ -40,16 +49,16 @@ class VaccinationsCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      
       children: [
         Image.asset(image),
         Container(
           margin: EdgeInsets.all(20),
-          child: Text(date,textAlign: TextAlign.left,
-                    style: GoogleFonts.comfortaa( 
-                        fontStyle: FontStyle.normal,
-                        fontWeight: FontWeight.w800,
-                        fontSize: 16)),
+          child: Text(date,
+              textAlign: TextAlign.left,
+              style: GoogleFonts.comfortaa(
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.w800,
+                  fontSize: 16)),
         ),
         Container(
           decoration: BoxDecoration(color: Color.fromRGBO(255, 223, 142, 100)),
@@ -57,16 +66,16 @@ class VaccinationsCard extends StatelessWidget {
             children: [
               Container(
                 margin: EdgeInsets.all(20),
-                child: Text(title,textAlign: TextAlign.left,
-                          style: GoogleFonts.comfortaa(
-                              fontStyle: FontStyle.normal,
-                              fontWeight: FontWeight.w800,
-                              fontSize: 16)),
+                child: Text(title,
+                    textAlign: TextAlign.left,
+                    style: GoogleFonts.comfortaa(
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w800,
+                        fontSize: 16)),
               ),
             ],
           ),
         ),
-
       ],
     );
   }
