5cc64489e18fe4282b34cd12d830a4d9a4dda266
Марат Пустовой <pustovoy31032003@mail.ru>
2022-03-30 20:21:55 +0300

    Возраст теперь меняется

--- lib/pages/ProfilePage/ProfilePage.dart
+++ lib/pages/ProfilePage/ProfilePage.dart
@@ -11,133 +11,141 @@ import 'package:pet_care/pages/providers/userprovider.dart';
 import 'package:pet_care/repository/profilespetsrepo.dart';
 import 'package:provider/provider.dart';
 
+DateTime dateToday =
+    DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
+DateTime dateBirth = DateTime.utc(2020, 10, 10);
+String age = ((dateToday.year - dateBirth.year) +
+    0.1 * (dateToday.month - dateBirth.month)).toStringAsFixed(1);
+
 class ProfilePage extends StatelessWidget {
-MyUser user;
+  MyUser user;
 
   @override
-  
   Widget build(BuildContext context) {
-  Future<MyUser> getUserData() => UserPreferences().getUser();
+    Future<MyUser> getUserData() => UserPreferences().getUser();
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
-        ChangeNotifierProvider(create: (_) => UserProvider()),
-      ],
-      child: 
-    FutureBuilder(
-      future: getUserData(),
-      builder: (context,snapshot) {
-        
-        switch (snapshot.connectionState) {
-                  case ConnectionState.none:
-                  case ConnectionState.waiting:
-                    return CircularProgressIndicator();
-                  default:
-                    if (snapshot.hasError)
-                      return Text('Error: ${snapshot.error}');
-                    else user=snapshot.data;
+        providers: [
+          ChangeNotifierProvider(create: (_) => AuthProvider()),
+          ChangeNotifierProvider(create: (_) => UserProvider()),
+        ],
+        child: FutureBuilder(
+            future: getUserData(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                case ConnectionState.waiting:
+                  return CircularProgressIndicator();
+                default:
+                  if (snapshot.hasError)
+                    return Text('Error: ${snapshot.error}');
+                  else
+                    user = snapshot.data;
 
-                      //UserPreferences().removeUser();
-                    
-                }
-        return ListView(children: [
-          
-          Container(
-            decoration:
-                BoxDecoration(color: Color.fromRGBO(255, 223, 142, 10), boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                blurRadius: 4,
-                offset: const Offset(0.0, 0.0),
-                spreadRadius: 0.0,
-              )
-            ]),
-            padding: EdgeInsets.all(10),
-            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              AvatarBlock(profilespets[1].name, profilespets[1].photo),
-              Column(
-                children: [
-                  Container(
-                      child: Icon(
-                        CupertinoIcons.add,
-                        size: 55,
-                        color: Colors.black,
-                      ),
-                      decoration: BoxDecoration(
-                          color: Color.fromRGBO(240, 240, 240, 1),
-                          //color: Color.fromRGBO(255, 223, 142, 10),
-                          border: Border.all(color: Colors.black),
-                          borderRadius: BorderRadius.all(Radius.circular(50))),
-                      height: 70,
-                      width: 70),
-                  Container(
-                    margin: EdgeInsets.all(10),
-                    child: Text(
-                      "Добавить",
-                      style: GoogleFonts.comfortaa(
-                          fontStyle: FontStyle.normal,
-                          fontWeight: FontWeight.w800,
-                          fontSize: 12),
-                    ),
-                  )
-                ],
-              )
-            ]),
-          ),
-          Container(
-            padding: EdgeInsets.all(10),
-            child: Text(
-              "Основные данные",
-              textAlign: TextAlign.center,
-              style: GoogleFonts.comfortaa(
-                  fontStyle: FontStyle.normal,
-                  fontWeight: FontWeight.w800,
-                  fontSize: 20),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.all(8),
-            decoration:
-                BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(10))),
-            padding: EdgeInsets.all(4),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                MainInfoBlock("Возраст", "1 год \n10 месяцев",
-                    Color.fromRGBO(131, 184, 107, 80)),
-                MainInfoBlock(
-                  "Вес",
-                  "15 кг",
-                  Color.fromRGBO(255, 223, 142, 10),
+                //UserPreferences().removeUser();
+
+              }
+              return ListView(children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: Color.fromRGBO(255, 223, 142, 10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 4,
+                          offset: const Offset(0.0, 0.0),
+                          spreadRadius: 0.0,
+                        )
+                      ]),
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        AvatarBlock(
+                            profilespets[1].name, profilespets[1].photo),
+                        Column(
+                          children: [
+                            Container(
+                                child: Icon(
+                                  CupertinoIcons.add,
+                                  size: 55,
+                                  color: Colors.black,
+                                ),
+                                decoration: BoxDecoration(
+                                    color: Color.fromRGBO(240, 240, 240, 1),
+                                    //color: Color.fromRGBO(255, 223, 142, 10),
+                                    border: Border.all(color: Colors.black),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(50))),
+                                height: 70,
+                                width: 70),
+                            Container(
+                              margin: EdgeInsets.all(10),
+                              child: Text(
+                                "Добавить",
+                                style: GoogleFonts.comfortaa(
+                                    fontStyle: FontStyle.normal,
+                                    fontWeight: FontWeight.w800,
+                                    fontSize: 12),
+                              ),
+                            )
+                          ],
+                        )
+                      ]),
                 ),
-                // MainInfoBlock("Порода", "Корги", Color.fromRGBO(131, 184, 107, 60)),
-                MainInfoBlock("Пол", "Мужской", Color.fromRGBO(129, 181, 217, 90)),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(10),
-            child: Text(
-              "Паспорт питомца",
-              textAlign: TextAlign.center,
-              style: GoogleFonts.comfortaa(
-                  fontStyle: FontStyle.normal,
-                  fontWeight: FontWeight.w800,
-                  fontSize: 20),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 0, top: 10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Passport(user.name, "30.01.2020", "Корги", "Рыжий",
-                    "Прививка от бешенства", "Нет"),
-              ],
-            ),
-          ),
-        ]);
-      }
-    ));
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    "Основные данные",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.comfortaa(
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w800,
+                        fontSize: 20),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  padding: EdgeInsets.all(4),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      MainInfoBlock("Возраст", age,
+                          Color.fromRGBO(131, 184, 107, 80)),
+                      MainInfoBlock(
+                        "Вес",
+                        "15 кг",
+                        Color.fromRGBO(255, 223, 142, 10),
+                      ),
+                      // MainInfoBlock("Порода", "Корги", Color.fromRGBO(131, 184, 107, 60)),
+                      MainInfoBlock(
+                          "Пол", "Мужской", Color.fromRGBO(129, 181, 217, 90)),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    "Паспорт питомца",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.comfortaa(
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w800,
+                        fontSize: 20),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 0, top: 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Passport(user.name, "30.01.2020", "Корги", "Рыжий",
+                          "Прививка от бешенства", "Нет"),
+                    ],
+                  ),
+                ),
+              ]);
+            }));
   }
 }
