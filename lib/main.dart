129df4ad0023a5c6c3b5a3f2dbebd6c3c17527d8
Tatayna-san <tany.pligynova@gmail.com>
2022-04-15 22:03:33 +0300

    На странице регистрации заменила иконки "конверик" на более логичные

--- lib/main.dart
+++ lib/main.dart
@@ -63,7 +63,7 @@ class MyApp extends StatelessWidget {
            '/welcome':(context) => WelcomeScreen()
           },
         title: 'PetCare',
-        initialRoute: '/welcome',
+        //initialRoute: '/welcome',
         /*
         routes: {
           '/test':(BuildContext context)=>ArticleListPage(),
